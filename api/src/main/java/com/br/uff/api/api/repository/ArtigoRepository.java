ad37a6f57042a3617b8f820af9cc31e71362a383
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-08-15 15:07:25 -0300

    Ajuste

--- api/src/main/java/com/br/uff/api/api/repository/ArtigoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.uff.api.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.uff.api.api.model.Artigo;
-
-public interface ArtigoRepository extends JpaRepository<Artigo, Long>{
-
-}
--- api/src/main/java/com/br/uff/api/api/repository/VolumeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.uff.api.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.uff.api.api.model.Volume;
-
-public interface VolumeRepository extends JpaRepository<Volume, Long>{
-
-}
