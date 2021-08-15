ef7ff0cf6f8cc3825fa9ebad9b92394e465e8c1d
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-08-15 13:40:28 -0300

    API pronta

---
+++ api/src/main/java/com/br/uff/api/api/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.br.uff.api.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.uff.api.api.model.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
---
+++ api/src/main/java/com/br/uff/api/api/repository/VolumeRepository.java
@@ -0,0 +1,9 @@
+package com.br.uff.api.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.uff.api.api.model.Volume;
+
+public interface VolumeRepository extends JpaRepository<Volume, Long>{
+
+}
